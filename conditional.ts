43505a4b428297ae60042a37aef26988e5b44994
Leonardo Tireck <leotireck@gmail.com>
2025-05-07 20:25:39 -0300

    add conditional type example

--- conditional.ts
+++ conditional.ts
@@ -12,9 +12,29 @@ let arrayElement: ElementType = someArray[2]; // "c" : string
 
 type GetElementType<T> = T extends any[] ? T[number] : never; // The extends here "asks" if the incoming T type is an array of any kind. And if it is, it will return the type of the specific element requested between the brackets, if it's not an array, it won't return.
 // This is how you define a conditional type on TS
-
 type Example1 = GetElementType<typeof someArray>;
-
 let example1: Example1 = "Hello World";
-
 // With this structure, we can pass the type of the elements of an array forward.
+
+// another scenario in which this might be useful is for type checking the return value of a function based on its inputs:
+type FullNamePerson = { firstName: string; lastName: string };
+
+type FullNameOrNothing<T> = T extends FullNamePerson ? string : never;
+
+function getFullName<T extends object>(person: T): FullNameOrNothing<T> {
+  if (
+    "firstName" in person &&
+    "lastName" in person &&
+    person.firstName &&
+    person.lastName
+  ) {
+    return `${person.firstName} ${person.lastName}` as FullNameOrNothing<T>;
+  }
+
+  throw new Error("No first name and/or last name found.");
+}
+
+const name1 = getFullName({ firstName: "Leo", lastName: "Tireck" }); // this will return string type
+console.log(name1);
+
+const name2 = getFullName({ firstName: "Leo" }); // this will return the never type and will crash the app
