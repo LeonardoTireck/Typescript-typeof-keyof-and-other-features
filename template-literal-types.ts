27419ad071a9e7548f9fcec584c1f1d5375110cd
Leonardo Tireck <leonardotireck@Leonardos-MacBook-Air.local>
2025-05-07 13:32:58 -0300

    add notes on template literal types

--- template-literal-types.ts
+++ template-literal-types.ts
@@ -2,7 +2,22 @@ type ReadPermissions = "no-read" | "read";
 type WritePermissions = "no-write" | "write";
 
 type FilePermissions = `${ReadPermissions}-${WritePermissions}`;
-// this forces TS to create a new string literal type
+// this forces TS to create a new string literal type (union)
 // if you hover over FilePermissions you'll see:
 // no-read-no-write | "no-read-write" | "read-no-write" | "read-write"
 // which is the combination with no repetition of the ReadPermissions type and the WritePermissions type
+
+// now lets say if you want to automatically create an event name (a method) for each property of the DataFile type
+
+type DataFile = {
+  data: string;
+  permissions: FilePermissions;
+};
+
+// this is a template literal type that creates a union of string literals and adds "Changed" to each property name of the DataFile type
+type DataFileEventNames = `${keyof DataFile}Changed`;
+
+// now we can use this to create a type that has a method for each property of the DataFile type with the "Changed" suffix
+type DataFileEvents = {
+  [Key in DataFileEventNames]: () => void;
+};
